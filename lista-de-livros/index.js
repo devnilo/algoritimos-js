4d571e1648dc035e19c0dc20a4e52a40b9b046b8
Danilo Araujo <dnlysid@gmail.com>
2022-02-09 08:45:45 -0300

    iniciando lista ordenada

---
+++ lista-de-livros/index.js
@@ -0,0 +1,23 @@
+/* Irei fazer a junção de duas listas e ordernar elas por preço do menor para o maior */
+
+const {edGalho, edFolha} = require('./arrays');
+
+function juntaListas(lista1, lista2) {
+    let listaFinal = [];
+
+    let posicaoAtualLista1 = 0;
+    let posicaoAtualLista2 = 0;
+
+    let atual = 0;
+
+    while (posicaoAtualLista1 < lista1.length && posicaoAtualLista2 < lista2.length) {
+        let produtoAtualLista1 = lista1[posicaoAtualLista1];
+        let produtoAtualLista2 = lista2[posicaoAtualLista2];
+
+        if (produtoAtualLista1.preco < produtoAtualLista2.preco) {
+            listaFinal[atual]
+        }
+    }
+
+    return listaFinal;
+}
