a1e8c390b5da9e495cca6af98909481a6fe8a6d4
Danilo Araujo <dnlysid@gmail.com>
2022-02-21 15:53:11 -0300

    finalizando merge na lista

--- lista-de-livros/merge/index.js
+++ lista-de-livros/merge/index.js
@@ -7,23 +7,23 @@ function mergeSort(array) {
         const parte1 = mergeSort(array.slice(0, meio));
         const parte2 = mergeSort(array.slice(meio, array.length));
 
-        array = ordena(parte1, parte2)
+        array = ordena(parte1, parte2);
     }
 
     return array;
 }
 
 function ordena(parte1, parte2) {
-    let posicaoAtualParte1 = 0;
-    let posicaoAtualParte2 = 0;
-    const resultado = [];
+    let posicaoAtualParte1 = 0 
+    let posicaoAtualParte2 = 0
+    const resultado = []
 
-    while (posicaoAtualParte1 < posicaoAtualParte1.length && posicaoAtualParte2 < posicaoAtualParte2.length) {
-        let produtoAtualParte1 = parte1[posicaoAtualParte1]; 
-        let produtoAtualParte2 = parte2[posicaoAtualParte2];
+    while (posicaoAtualParte1 < parte1.length && posicaoAtualParte2 < parte2.length) {
+        let produtoAtualParte1 = parte1[posicaoAtualParte1]
+        let produtoAtualParte2 = parte2[posicaoAtualParte2]
 
         if (produtoAtualParte1.preco < produtoAtualParte2.preco) {
-            resultado.push(posicaoAtualParte1)
+            resultado.push(produtoAtualParte1)
             posicaoAtualParte1++
         } else {
             resultado.push(produtoAtualParte2)
@@ -31,9 +31,9 @@ function ordena(parte1, parte2) {
         }
     }
 
-    return resultado.concat(posicaoAtualParte1 < parte1.length 
-        ? parte1.slice(posicaoAtualParte1)
-        : parte2.slice(posicaoAtualParte2))
+    return resultado.concat(posicaoAtualParte1 < parte1.length
+    ? parte1.slice(posicaoAtualParte1)
+    : parte2.slice(posicaoAtualParte2))
 }
 
 console.log(mergeSort(listaLivros));
