8af0eb0f36a96e1b30d22d1edbcd04722a69d533
Danilo Araujo <dnlysid@gmail.com>
2022-03-02 11:08:03 -0300

    criando busca binária

---
+++ lista-de-livros/busca-binaria/busca.js
@@ -0,0 +1,22 @@
+const listaLivros = require('./arrayOrdenado');
+
+function busca(array, de, ate, valorBuscado) {
+    // diferente do pivo, quando fizermos uma busca binária sempre começaremos do meio
+    const meio = Math.floor((de + ate) / 2);
+    const atual = array[meio];
+
+     if (valorBuscado === atual.preco) {
+         return meio;
+     }
+
+     if (valorBuscado < atual.preco) {
+        return busca(array, de, meio - 1, valorBuscado);
+     }
+
+     if (valorBuscado > atual.preco) {
+        return busca(array, meio + 1, ate, valorBuscado);
+     }
+}
+
+console.log(busca(listaLivros, 0, listaLivros.length -1, 40)) /* o quarto parametro é o valor que 
+                                                                queremos buscar */ 
